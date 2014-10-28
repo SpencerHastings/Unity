736b1265f0f0c58733472069b1b4564ca4eb2483
SpencerHastings <shastings007@gmail.com>
2014-10-28 15:48:51 -0700

    Added Git Ignore

---
+++ Test/Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+
+	Vector2 velocity;
+	float movespeed = 1;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		velocity = new Vector2 (Input.GetAxis ("Horizontal") * movespeed, Input.GetAxis ("Vertical") * movespeed);
+		transform.position += new Vector3 (velocity.x, velocity.y, 0);
+	
+	}
+}
